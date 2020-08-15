9bd55f9fec8356929b72694977f83eba4e9ec7a6
rene.inomata <rene841@icloud.com>
2020-08-15 13:57:16 +0000

    sai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show]
 
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
-
   def new
     @user = User.new
   end
