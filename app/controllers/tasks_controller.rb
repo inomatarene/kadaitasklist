d2417733b97726a0122f71824b2d61701937fa31
rene.inomata <rene841@icloud.com>
2020-08-15 12:48:12 +0000

    add bootstrap design3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,17 +15,19 @@ class TasksController < ApplicationController
   end
   
   def new
+    @task = Task.new
   end
 
   def create
     @task = current_user.tasks.build(task_params)
+    
     if @task.save
       flash[:success] = 'メッセージを投稿しました。'
-      redirect_to root_url
+      redirect_to @task
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
-      render 'tasks/index'
+      render :new
     end
   end
   
